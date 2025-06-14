3c9025d7de05db82c836464e892e77226eca1ff1
diffghjkl <diffghjkl@foxmail.com>
2025-06-14 19:27:36 +0800

    'deploy'

--- pages/about/site.md
+++ pages/about/site.md
@@ -1,7 +1,12 @@
 ---
-title: 关于站点
+title: 关于博客
 ---
 
-Valaxy + valaxy-theme-yun Demo Site
+- 2020/03 - 《梦的开始》——博客建立，使用 `Wordpress` 搭建.
+> 后因数据丢失&资金不足，博客关闭.
 
-- Default Theme: [valaxy-theme-yun](https://github.com/YunYouJun/valaxy/blob/main/packages/valaxy-theme-yun/)
+- 2021/10/30 - 重启博客，因 Wordpress 维护成本较高（指流畅度），更换为 `Typecho` 搭建.
+- 2022/05/07 - 为节约资金，选择 静态博客框架 中的 `Hexo` .
+- 2025/05/11 - 由于旧站存在部分问题，选用 `Valaxy` 搭建.
+
+博主信息请见 [关于博主](https://blog.dmoe.top/about/) .
--- valaxy.config.ts
+++ valaxy.config.ts
@@ -1,6 +1,6 @@
 import type { UserThemeConfig } from 'valaxy-theme-yun'
 import { defineValaxyConfig } from 'valaxy'
-import { addonWaline } from "valaxy-addon-waline";
+import { addonTwikoo } from 'valaxy-addon-twikoo';
 import { addonBangumi } from 'valaxy-addon-bangumi';
 import { addonFace } from 'valaxy-addon-face'
 
@@ -78,24 +78,14 @@ export default defineValaxyConfig<UserThemeConfig>({
       enable: true
     },
   },
-  // 设置 valaxy-addon-waline 配置项
   addons: [
-    addonWaline({
-      serverURL: 'https://bpl.dmoe.top/',
-      locale: {
-        placeholder: "烦请友善交流~",
-      },
-      reaction: [],
-      comment: true, // 评论数统计
-      pageview: true, // 浏览量统计
-      highlighter: true,
-      texRenderer: true,
-      emoji: [
-        '//unpkg.com/@waline/emojis@1.2.0/bilibili',
-        '//unpkg.com/@waline/emojis@1.2.0/bmoji',
-        '//unpkg.com/@waline/emojis@1.2.0/tieba',
-        '//unpkg.com/@waline/emojis@1.2.0/qq',
-      ],
+      // 设置 valaxy-addon-twikoo 配置项
+    addonTwikoo({
+      envId: 'https://bpl1.dmoe.top/', // 腾讯云环境填 envId；Vercel 环境填地址（https://xxx.vercel.app）
+      el: '#tcomment', // 容器元素
+  // region: 'ap-guangzhou', // 环境地域，默认为 ap-shanghai，腾讯云环境填 ap-shanghai 或 ap-guangzhou；Vercel 环境不填
+  // path: location.pathname, // 用于区分不同文章的自定义 js 路径，如果您的文章路径不是 location.pathname，需传此参数
+      lang: 'zh-CN', // 用于手动设定评论区语言，支持的语言列表 https://github.com/twikoojs/twikoo/blob/main/src/client/utils/i18n/index.js
     }),
     // 追番
     addonBangumi({
