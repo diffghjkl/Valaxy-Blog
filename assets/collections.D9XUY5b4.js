1a63dda61f5f2b864e8eb5b409f265407797e5e8
diffghjkl <diffghjkl@users.noreply.github.com>
2025-09-13 11:42:46 +0000

    deploy: 92d55d20e112a2d4e4735a60ffd95ff6fdcdf1c8

---
+++ assets/collections.D9XUY5b4.js
@@ -0,0 +1,1 @@
+import"./chunks/@vueuse/motion.CZ-PQs79.js";import{u as l}from"./chunks/vue-router.8hGbrpip.js";import{M as t}from"./framework.DzSIVGYh.js";const e=[];function a(){return{collections:t(()=>e)}}function p(){const o=l(),n=t(()=>o.path.startsWith("/collections/")?o.path.split("/")[2]:"");return{collection:t(()=>e.find(c=>c.key===n.value))}}export{a,p as u};
