92d55d20e112a2d4e4735a60ffd95ff6fdcdf1c8
diffghjkl <diffghjkl@foxmail.com>
2025-09-13 19:41:25 +0800

    'deploy'

--- components/YunFooter.vue
+++ components/YunFooter.vue
@@ -7,7 +7,7 @@ useScriptTag('//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js')
 
 <template>
   <YunFooter>
-     <!-- 自定义页脚内容 -->
+    <!-- 自定义页脚内容 -->
     <div>本站总访问量 <span id="busuanzi_value_site_pv" /> 次</div>
     <div>本站访客数 <span id="busuanzi_value_site_uv" /> 人次</div>
 
